3fb988daf14b24bae806c36fc355c8a77a916e7d
Ekaterina Belova <76008172+kbelovaa@users.noreply.github.com>
2022-11-06 00:33:52 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const $form = document.getElementById('form');
+const $answer = document.getElementById('answer');
+
+function aliceDecrypt(message) {
+  let isNotDecrypted = true;
+
+  if (message.length > 100000) {
+    return 'The message is too long';
+  }
+
+  do {
+    isNotDecrypted = false;
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === message[i + 1]) {
+        message = message.slice(0, i) + message.slice(i + 2);
+        isNotDecrypted = true;
+      }
+    }
+  } while (isNotDecrypted);
+
+  return message;
+}
+
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  $answer.innerText = aliceDecrypt($form.message.value.trim());
+});
